423ea4d96369c22d674106cb75f87cbbebf8c17b
Joseph Lord <joseph@human-friendly.com>
2014-08-06 16:35:55 +0100

    Initial Commit

---
+++ London Swift Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  London Swift Demo
+//
+//  Created by Joseph on 06/08/2014.
+//  Copyright (c) 2014 Human Friendly Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ London Swift DemoTests/London_Swift_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  London_Swift_DemoTests.swift
+//  London Swift DemoTests
+//
+//  Created by Joseph on 06/08/2014.
+//  Copyright (c) 2014 Human Friendly Ltd. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class London_Swift_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
