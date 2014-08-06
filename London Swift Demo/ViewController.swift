ff40f5e2c9d5445e572f44952bda6ed69d9c3a66
Joseph Lord <joseph@human-friendly.com>
2014-08-07 00:13:44 +0100

    Add UIDynamics demo.

--- London Swift Demo/ViewController.swift
+++ London Swift Demo/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    // Need to have something that owns the behaviours beyond the function life.
+    var animator:UIDynamicAnimator!
+    
+    required init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let blueV = UIView(frame: CGRect(x: 20, y: 50, width: 80, height: 80))
+        blueV.backgroundColor = UIColor.blueColor()
+        
+        // The main view will retain the subview
+        self.view.addSubview(blueV)
+        self.animator = UIDynamicAnimator(referenceView: view)
+        
+        let gravityBehaviour = UIGravityBehavior(items: [blueV])
+        animator.addBehavior(gravityBehaviour)
+        
+        let collisionBehaviour = UICollisionBehavior(items: [blueV])
+        collisionBehaviour.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collisionBehaviour)
+        
+        let itemBehaviour = UIDynamicItemBehavior(items: [blueV])
+        itemBehaviour.addLinearVelocity(CGPoint(x: 200, y: 30), forItem: blueV)
+        itemBehaviour.elasticity = 0.99 // Bouncyness
+        itemBehaviour.addAngularVelocity(0.5, forItem: blueV)
+        animator.addBehavior(itemBehaviour)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
